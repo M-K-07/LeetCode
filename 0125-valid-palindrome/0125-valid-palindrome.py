755d2160ced463d1f22f4a5c762a9013dfce5bbb
Murali Krishna <156583019+M-K-07@users.noreply.github.com>
2025-01-22 23:29:39 +0530

    Time: 13 ms (16.75%), Space: 23.8 MB (7.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,7 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         lst=[]
         for i in s:
-            if i.isalnum():
+            if i.lower() in 'abcdefghijklmnopqrstuvwxyz1234567890':
                 lst.append(i.lower())
         if lst==lst[::-1]:
             return True
