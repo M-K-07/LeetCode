9a994d5a0c42e3a2ff11f518be45fcc9541a831e
Murali Krishna <156583019+M-K-07@users.noreply.github.com>
2025-03-20 22:35:22 +0530

    Time: 4 ms (4.64%), Space: 17.8 MB (62.15%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -7,13 +7,7 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         temp1,temp2=list1,list2
         lst=[]
-        while (temp1 != None and temp2 != None):
-            if temp1.val<temp2.val:
-                lst.append(temp1.val)
-                temp1=temp1.next
-            else:
-                lst.append(temp2.val)
-                temp2=temp2.next
+        
         while(temp1!=None):
             lst.append(temp1.val)
             temp1=temp1.next
@@ -22,7 +16,26 @@ class Solution:
             temp2=temp2.next
         new_lst=ListNode(0)
         temp=new_lst
-        for i in lst:
+        for i in sorted(lst):
             temp.next=ListNode(i)
             temp=temp.next
         return new_lst.next
+        # while (temp1 != None and temp2 != None):
+        #     if temp1.val<temp2.val:
+        #         lst.append(temp1.val)
+        #         temp1=temp1.next
+        #     else:
+        #         lst.append(temp2.val)
+        #         temp2=temp2.next
+        # while(temp1!=None):
+        #     lst.append(temp1.val)
+        #     temp1=temp1.next
+        # while(temp2!=None):
+        #     lst.append(temp2.val)
+        #     temp2=temp2.next
+        # new_lst=ListNode(0)
+        # temp=new_lst
+        # for i in lst:
+        #     temp.next=ListNode(i)
+        #     temp=temp.next
+        # return new_lst.next
