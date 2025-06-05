377ab6b4c799b47447e01ff48103b097e1592f5e
Murali Krishna <156583019+M-K-07@users.noreply.github.com>
2025-06-05 22:51:09 +0530

    Time: 11 ms (72.66%), Space: 17.6 MB (97.54%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        d={}
+        for i in s:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for i in t:
+            if i in d:
+                d[i]-=1
+            else:
+                d[i]=1
+        for item in d:
+            if d[item]!=0:
+                return False
+        return True
